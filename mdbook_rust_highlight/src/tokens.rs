f65bd81c1509d58af19d49464a223d27d9f6d473
sagi21805 <sagi21805@gmail.com>
2025-09-27 17:45:45 +0300

    Added a spanned token struct to save only the position of the token

--- mdbook_rust_highlight/src/tokens.rs
+++ mdbook_rust_highlight/src/tokens.rs
@@ -1,35 +1,60 @@
+use std::cmp::{Ordering, Reverse};
+
 use crate::highlighter::RustHighlighter;
-use mdbook_rust_highlight_derive::{add_try_method, register_variants};
+use mdbook_rust_highlight_derive::{RegisterVariants, add_try_method};
 use strum_macros::AsRefStr;
 
-/// Token mapping with
-#[register_variants]
-#[derive(AsRefStr, Debug, Clone)]
+#[derive(AsRefStr, RegisterVariants, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TokenTag {
-    Keyword(usize),
-    Ident(usize),
-    LitStr(usize),
-    LitNum(usize),
-    LitBool(usize),
-    EndOfToken(usize),
-    Function(usize),
-    SelfToken(usize),
-    Macro(usize),
-    Type(usize),
-    Enum(usize),
-    Segment(usize),
-    Comment(usize),
-    LifeTime(usize),
-    NeedIdentification(usize),
-    Boring(usize),
+    Keyword,
+    Ident,
+    LitStr,
+    LitNum,
+    LitBool,
+    Function,
+    SelfToken,
+    Macro,
+    Type,
+    Enum,
+    Segment,
+    Comment,
+    LifeTime,
+    NeedIdentification,
+    Boring,
+    EndOfToken,
 }
 
 impl ToString for TokenTag {
     fn to_string(&self) -> String {
         match self {
-            Self::Boring(_) => String::from("<span class=\"boring\">"),
-            Self::EndOfToken(_) => String::from("</span>"),
+            Self::Boring => String::from("<span class=\"boring\">"),
+            Self::EndOfToken => String::from("</span>"),
             _ => format!("<span class=\"hlrs-{}\">", self.as_ref()),
         }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct SpannedToken {
+    pub(crate) kind: TokenTag,
+    pub(crate) start: usize,
+    pub(crate) end: usize,
+}
+
+impl PartialOrd for SpannedToken {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        (self.start, Reverse(self.end), self.kind.clone()).partial_cmp(&(
+            other.start,
+            Reverse(other.end),
+            other.kind.clone(),
+        ))
+    }
+}
+
+impl Ord for SpannedToken {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.start, Reverse(self.end), self.kind.clone())
+            .cmp(&(other.start, Reverse(other.end), other.kind.clone()))
+            .then(Ordering::Greater)
     }
 }
