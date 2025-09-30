979c10f9b371ee0d8a36b6d912238fc4f8ac0bfa
sagi21805 <sagi21805@gmail.com>
2025-09-30 22:13:33 +0300

    path now also records tag ident if known

--- mdbook_rust_highlight/src/highlighter/path.rs
+++ mdbook_rust_highlight/src/highlighter/path.rs
@@ -3,7 +3,7 @@ use syn::{Path, PathArguments, PathSegment, QSelf, spanned::Spanned};
 
 use crate::{highlighter::RustHighlighter, tokens::TokenTag};
 
-impl<'ast> RustHighlighter<'ast> {
+impl<'a, 'ast> RustHighlighter<'a, 'ast> {
     pub(crate) fn register_path_argument(&mut self, token: &'ast PathArguments) {
         match token {
             PathArguments::Parenthesized(token) => {
@@ -16,9 +16,20 @@ impl<'ast> RustHighlighter<'ast> {
         }
     }
 
-    pub(crate) fn register_path_segment(&mut self, token: &'ast PathSegment, tag: TokenTag) {
-        self.register_tag(&token.ident, tag);
+    pub(crate) fn register_path_segment(
+        &mut self,
+        token: &'ast PathSegment,
+        tag: Option<TokenTag>,
+    ) {
         self.register_path_argument(&token.arguments);
+        match tag {
+            None => {
+                self.register_tag(&token.ident, TokenTag::NeedIdentification);
+                self.unidentified
+                    .insert(token.span().byte_range().start, &token);
+            }
+            Some(tag) => self.register_ident(&token.ident, tag),
+        }
     }
 
     #[add_try_method]
@@ -35,22 +46,14 @@ impl<'ast> RustHighlighter<'ast> {
     /// - `last:` - Optional tag to put for the last item of the path.
     ///
     /// TODO ADD DOCUMENTATION AND PLAN WHAT HAPPENS IF NONE IS GIVEN
-    pub(crate) fn register_path(&mut self, token: &'ast Path, last: Option<TokenTag>) {
+    pub(crate) fn register_path(&mut self, token: &'ast Path, last_tag: Option<TokenTag>) {
         let mut segment_iter = token.segments.iter().rev();
         let last_segment = segment_iter.next();
-        for segment in &token.segments {
+        for segment in segment_iter {
             self.register_segment_tag(segment);
         }
-        match last_segment {
-            Some(segment) => match last {
-                Some(tag) => self.register_path_segment(segment, tag),
-                None => {
-                    self.register_path_segment(segment, TokenTag::NeedIdentification);
-                    self.unidentified
-                        .insert(segment.span().byte_range().start, &segment);
-                }
-            },
-            None => {}
+        if let Some(seg) = last_segment {
+            self.register_path_segment(seg, last_tag);
         }
     }
 }
