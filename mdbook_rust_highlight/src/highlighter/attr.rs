5688c16a01dfc26196868fe4ba20f7ff76f3ac2b
sagi <sagi21805@gmail.com>
2025-10-19 09:10:09 +0300

    addded a register attribute function

--- mdbook_rust_highlight/src/highlighter/attr.rs
+++ mdbook_rust_highlight/src/highlighter/attr.rs
@@ -5,31 +5,50 @@ use crate::{highlighter::RustHighlighter, tokens::TokenTag};
 impl<'a, 'ast> RustHighlighter<'a, 'ast> {
     pub(crate) fn register_attributes(&mut self, token: &'ast Vec<Attribute>) {
         for attr in token {
-            self.register_attribute_tag(attr);
-            match attr.style {
-                AttrStyle::Outer => match &attr.meta {
-                    Meta::Path(path) => {
-                        self.register_path(path, Some(TokenTag::Function));
+            self.register_attribute(attr);
+        }
+    }
+
+    pub(crate) fn register_attribute(&mut self, token: &'ast Attribute) {
+        // Distinguish between a comment or docs to an attribute.
+        if token.pound_token.span.byte_range().len() == 1 {
+            self.register_attribute_tag(&token.pound_token);
+            self.register_attribute_tag(&token.bracket_token.span.join());
+        }
+        match token.style {
+            AttrStyle::Outer => match &token.meta {
+                Meta::Path(path) => {
+                    self.register_path(path, Some(TokenTag::Const));
+                }
+                Meta::List(list) => {
+                    if list.path.get_ident().unwrap().to_string() == "unsafe" {
+                        self.register_keyword_tag(&list.path);
+                    } else {
+                        self.register_path(&list.path, Some(TokenTag::Const));
                     }
-                    Meta::List(list) => {
-                        if list.path.get_ident().unwrap().to_string() == "unsafe" {
-                            self.register_keyword_tag(&list.path);
-                        } else {
-                            self.register_path(&list.path, Some(TokenTag::Function));
-                        }
-                        let ident_list = syn::parse2::<IdentList>(list.tokens.clone());
-                        match ident_list {
-                            Ok(list) => {
-                                for item in &list.idents {
-                                    self.register_type_tag(item);
-                                }
+                    let ident_list = syn::parse2::<IdentList>(list.tokens.clone());
+                    match ident_list {
+                        Ok(list) => {
+                            for item in &list.idents {
+                                self.register_type_tag(item);
                             }
-                            _ => {}
                         }
+                        _ => {}
                     }
-                    Meta::NameValue(name_val) => {}
-                },
-                AttrStyle::Inner(_) => {}
+                }
+                Meta::NameValue(name_val) => {
+                    self.register_path(&name_val.path, Some(TokenTag::Ident));
+                    self.register_expr(&name_val.value);
+                }
+            },
+            AttrStyle::Inner(not) => {
+                self.register_attribute_tag(&not);
+                match &token.meta {
+                    Meta::Path(path) => {
+                        self.register_path(path, Some(TokenTag::Const));
+                    }
+                    _ => {}
+                }
             }
         }
     }
