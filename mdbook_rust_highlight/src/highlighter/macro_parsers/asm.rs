c7bc6e8758814af1b893877f20318847908274c5
sagi <sagi21805@gmail.com>
2025-11-06 09:37:26 +0200

    added structures for all inline asm with empty parse functions

--- mdbook_rust_highlight/src/highlighter/macro_parsers/asm.rs
+++ mdbook_rust_highlight/src/highlighter/macro_parsers/asm.rs
@@ -1,14 +1,32 @@
-use syn::{Ident, LitStr, Token, parse::Parse, punctuated::Punctuated, token::Paren};
+use syn::{
+    Expr, ExprPath, Ident, LitStr, Stmt, Token,
+    parse::Parse,
+    punctuated::Punctuated,
+    token::{Brace, Paren},
+};
 
 use super::ident_list::IdentList;
 
-pub struct AsmInputs {
+pub struct AsmArgs {
     inputs: Punctuated<AsmInput, Token![,]>,
+}
+
+impl Parse for AsmArgs {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
 }
 
 pub enum AsmInput {
     Instruction(LitStr),
     Options(AsmOptions),
+    RegOperand(RegOperand),
+}
+
+impl Parse for AsmInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
 }
 
 /// Structs represents the options on asm! macro `options(nostack, noreturn)`
@@ -35,5 +53,104 @@ impl Parse for AsmOptions {
             paren,
             options,
         })
+    }
+}
+
+/// The reg in `in(reg)` or the 'rax' in `out("rax")`
+pub enum RegSpec {
+    RegClass(Ident),
+    ExplicitReg(LitStr),
+}
+
+impl Parse for RegSpec {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+/// Inline asm operand directive : `in(reg) 3`
+pub struct OperandDirective {
+    pub directive: Ident,
+    pub paren: Paren,
+    pub reg: RegSpec,
+    pub expr: Expr,
+}
+
+impl Parse for OperandDirective {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+pub struct OperandDualDirective {
+    pub directive: Ident,
+    pub paren: Paren,
+    pub reg: RegSpec,
+    pub expr: Expr,
+    pub expr2: Option<(Token![=>], Expr)>,
+}
+
+impl Parse for OperandDualDirective {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+pub struct ExprSym {
+    pub sym_token: Ident,
+    pub expr: ExprPath,
+}
+
+impl Parse for ExprSym {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+pub struct ExprConstAsm {
+    pub const_token: Ident,
+    pub expr: Expr,
+}
+
+impl Parse for ExprConstAsm {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+pub struct Label {
+    pub label_token: Ident,
+    pub brackets: Brace,
+    pub stmt: Stmt,
+}
+
+impl Parse for Label {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+pub enum ExprOperand {
+    Directive(OperandDirective),
+    DualDirective(OperandDualDirective),
+    Symbol(ExprSym),
+    Constant(ExprConstAsm),
+    Label(Label),
+}
+
+impl Parse for ExprOperand {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
+}
+
+pub struct RegOperand {
+    pub param_name_eq: Option<(Ident, Token![=])>,
+    pub expr: ExprOperand,
+}
+
+impl Parse for RegOperand {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        todo!()
     }
 }
